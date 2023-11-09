21dfe1ca834b5fb9dc10499660c51e4a1326433d
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-11-09 16:25:56 +0700

    Update Kasus25.java

--- Kasus25.java
+++ Kasus25.java
@@ -1,8 +1,11 @@
+import java. util.Scanner;
+
 public class Kasus25 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int batas = 5;
-
+        
         // Loop luar untuk iterasi nilai pertama
         for (int i = 1; i <= batas; i++) {
 
